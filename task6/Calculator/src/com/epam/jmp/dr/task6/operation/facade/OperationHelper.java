fd49f0a2a235d76f52be5c5015613536b44fa1ef
Nikopol <fuckyou@email.com>
2016-08-20 20:10:56 +0300

    task6 commit

---
+++ task6/Calculator/src/com/epam/jmp/dr/task6/operation/facade/OperationHelper.java
@@ -0,0 +1,14 @@
+package com.epam.jmp.dr.task6.operation.facade;
+
+public class OperationHelper {
+	
+	public static float performOperation(OperationType type, float a, float b)
+	{
+		switch(type)
+		{
+		case DIFF:
+			
+		}
+	}
+
+}
---
+++ task6/Calculator/src/com/epam/jmp/dr/task6/operation/facade/OperationType.java
@@ -0,0 +1,10 @@
+package com.epam.jmp.dr.task6.operation.facade;
+
+public enum OperationType {
+	
+	DIFF,
+	DIV,
+	SUM,
+	MUL
+
+}
