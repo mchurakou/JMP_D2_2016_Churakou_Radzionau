4f9f0b3906a91dbc286b00698d5043fa8eaa357f
Dzmitry Radzionau <h311k4t@gmail.com>
2016-09-13 12:18:02 +0300

    task 10

---
+++ task10/DBGenerator/src/main/java/com/epam/jmp/dr/task10/DBHandler.java
@@ -0,0 +1,10 @@
+package com.epam.jmp.dr.task10;
+
+public class DBHandler {
+	
+	private static String USER = "root";
+	private static String PASSWORD = "root";
+	private static String URL = "jdbc:mysql://localhost:3306/task10";
+	
+
+}
