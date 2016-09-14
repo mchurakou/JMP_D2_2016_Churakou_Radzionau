b5791bc7eda74e1a2640fc98cbd419c79b3be648
Nikopol <fuckyou@email.com>
2016-09-14 20:23:48 +0300

    task 10

--- task10/DBGenerator/src/main/java/com/epam/jmp/dr/task10/DBHandler.java
+++ task10/DBGenerator/src/main/java/com/epam/jmp/dr/task10/DBHandler.java
@@ -12,8 +12,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 
 public class DBHandler {
@@ -60,20 +63,86 @@ public class DBHandler {
 	private static String INSERT_USER = "INSERT INTO users (name, surname, birthdate) VALUES(?, ?, ?);";
 	private static String INSERT_FRIENDSHIP = "INSERT INTO friendships values(?, ?, ?);";
 	private static String GET_USERS_COUNT = "SELECT COUNT(*) as users_count FROM users;";
+	private static String GET_USER_FRIENDSHIPS = "SELECT userid2 AS friend FROM friendships WHERE userid1 = ?;";
+	
 	
 	public static void generateFriendships()
 	{
+		System.out.println("Creating friendships....");
 		try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
 				PreparedStatement insertFriendshipStatement = conn.prepareStatement(INSERT_FRIENDSHIP);
-				Statement countUsersStatement = conn.createStatement()) {
+				Statement countUsersStatement = conn.createStatement();
+				PreparedStatement getUserFriendships = conn.prepareStatement(GET_USER_FRIENDSHIPS);
+				) {
 			
 			ResultSet usersCountSet = countUsersStatement.executeQuery(GET_USERS_COUNT);
 			usersCountSet.first();
-			int usersCount = usersCountSet.getInt("users_count");
+			int usersCount = usersCountSet.getInt("users_count");			
 			
+			for(int i = 0; i < 70000; i++)
+			{
+				int userid1 = randBetween(1, usersCount);
+				int userid2 = randBetween(1, usersCount);
+				while(userid1 == userid2)
+				{
+					userid2 = randBetween(1, usersCount);
+				}
+				getUserFriendships.setInt(1, userid1);
+				ResultSet usersFriends = getUserFriendships.executeQuery();
+				List<Integer> friendsArray = new ArrayList<>();
+				while(usersFriends.next())
+				{
+					friendsArray.add(usersFriends.getInt("friend"));
+				}
+				if(friendsArray.isEmpty())
+				{
+					Timestamp timestamp = getRandomDate(2010, 2016);
+					insertFriendshipStatement.setInt(1, userid1);
+					insertFriendshipStatement.setInt(2, userid2);
+					insertFriendshipStatement.setTimestamp(3, timestamp);
+					insertFriendshipStatement.executeUpdate();
+					insertFriendshipStatement.setInt(1, userid2);
+					insertFriendshipStatement.setInt(2, userid1);
+					insertFriendshipStatement.setTimestamp(3, timestamp);
+					insertFriendshipStatement.executeUpdate();
+				}
+				else
+				{
+					boolean hasFriendshipithUser2 = false;
+					do
+					{
+						hasFriendshipithUser2 = false;
+						for(Integer usr2ID : friendsArray)
+						{
+							if(usr2ID == userid2)
+							{
+								userid2 = randBetween(1, usersCount);
+								while(userid1 == userid2)
+								{
+									userid2 = randBetween(1, usersCount);
+								}
+								hasFriendshipithUser2 = true;
+								break;
+							}
+						}
+						
+					} while(hasFriendshipithUser2);
+					
+					
+					Timestamp timestamp = getRandomDate(2010, 2016);
+					insertFriendshipStatement.setInt(1, userid1);
+					insertFriendshipStatement.setInt(2, userid2);
+					insertFriendshipStatement.setTimestamp(3, timestamp);
+					insertFriendshipStatement.executeUpdate();
+					insertFriendshipStatement.setInt(1, userid2);
+					insertFriendshipStatement.setInt(2, userid1);
+					insertFriendshipStatement.setTimestamp(3, timestamp);
+					insertFriendshipStatement.executeUpdate();
+				}
+				
+			}
 			
-			
-			
+			getUserFriendships.close();
 			countUsersStatement.close();
 			insertFriendshipStatement.close();
 			conn.close();
@@ -82,10 +151,12 @@ public class DBHandler {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("Creating friendships finished");
 	}
 	
 	public static void generateUsers()
 	{
+		System.out.println("Creating users....");
 		try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
 				PreparedStatement statement = conn.prepareStatement(INSERT_USER);) {
 			
@@ -102,7 +173,7 @@ public class DBHandler {
 					
 					statement.setString(1, name);
 					statement.setString(2, surname);
-					statement.setDate(3, getRandomDate(1950, 2000));
+					statement.setDate(3, new Date(getRandomDate(1950, 2000).getTime()));
 					
 					statement.executeUpdate();
 					
@@ -120,9 +191,11 @@ public class DBHandler {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("Creating users finished");
 	}
 
 	public static void createTables() {
+		System.out.println("Creating tables...");
 		try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
 				Statement statement = conn.createStatement();) {
 			
@@ -140,25 +213,27 @@ public class DBHandler {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("Creating tables finished");
 	}
 	
-	private static java.sql.Date getRandomDate(int start, int end)
+	private static java.sql.Timestamp getRandomDate(int start, int end)
 	{
 		GregorianCalendar gc = new GregorianCalendar();
         int year = randBetween(start, end);
         gc.set(GregorianCalendar.YEAR, year);
         int dayOfYear = randBetween(1, gc.getActualMaximum(GregorianCalendar.DAY_OF_YEAR));
         gc.set(GregorianCalendar.DAY_OF_YEAR, dayOfYear);
-        /*int hour = randBetween(0, 23);
-        gc.set(hour, GregorianCalendar.HOUR_OF_DAY);
+        int hour = randBetween(0, 23);
+        //gc.set(hour, GregorianCalendar.HOUR_OF_DAY);
         int minute = randBetween(0, 59);
-        gc.set(minute, GregorianCalendar.MINUTE);
+        //gc.set(minute, GregorianCalendar.MINUTE);
         int second = randBetween(0, 59);
-        gc.set(second, GregorianCalendar.SECOND);*/
+        //gc.set(second, GregorianCalendar.SECOND);
         
         
-		java.sql.Date date = new java.sql.Date(gc.getTimeInMillis());
-        return date;
+		//java.sql.Date date = new java.sql.Date(gc.getTimeInMillis());
+        Timestamp timestamp = new Timestamp(gc.getTimeInMillis());
+        return timestamp;
 	}
 	
 	private static int randBetween(int start, int end) {
